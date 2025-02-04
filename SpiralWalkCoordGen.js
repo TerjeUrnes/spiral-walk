b5b02143a470012188b53bfe8dd8c8ae597eeb18
Terje Urnes <terje@urnes.net>
2025-02-05 00:27:38 +0100

    Added more options and some tests

--- SpiralWalkCoordGen.js
+++ SpiralWalkCoordGen.js
@@ -11,16 +11,44 @@ export class SpiralWalkCoordGen {
         return this._instance;
     }
 
-    _startX = 0;
-    _startY = 0;
-    _isIncludingStartCoor = true;
-    _boardWidth = 11;
-    _boardHeight = 11;
-    _maxCircles = null;
-    _hitOneBorder = false;
-    _hitAllBorders = true;
-    _includeCoordsOutsideBorders = false;
-    _circle;
+    _startCoord = {
+        x: 0, 
+        y: 0, 
+        includeInIteration: true
+    }
+    _stopCondition = {
+        maxCircles: false, 
+        reachedFirstBorder: false, 
+        reachedAllBorders: true, 
+        reachedIterationCount: 10000
+    }
+    _filter = {
+        useCustomFunc: false,
+        customFunc: () => { return true; }    
+    }
+    _border = {
+        x: -10,
+        y: -10,
+        width: 21,
+        height: 21,
+        includeCoordsOutside: false,
+        leftVerticalX: -10,
+        rightVerticalX: 10,
+        topHorizontalY: -10,
+        bottomHorizontalY: 10
+    }
+    _walking = {
+        direction: "cw"
+    }
+
+    _circleCount;
+    _coordsCount;
+    _spiralBorder = {
+        leftX: 0,
+        rightX: 0,
+        topY: 0,
+        bottomY: 0
+    }
 
     static set StartCoord({
         x = null,
@@ -39,35 +67,151 @@ export class SpiralWalkCoordGen {
         y = null,
         includeInIteration = null
     } = {}) {
-        if (x != null) { this._startX = x; }
-        if (y != null) { this._startY = y; }
-        if (includeInIteration != null) { this._isIncludingStartCoor = includeInIteration; }
+        if (x != null) { this._startCoord.x = x; }
+        if (y != null) { this._startCoord.y = y; }
+        if (includeInIteration != null) { 
+            this._startCoord.includeInIteration = includeInIteration; 
+        }
     }
 
     static set StopCondition({
         maxCircles = null,
-        hitOneBorder = false,
-        hitAllBorders = false,
-        includeCoordsOutsideBorders = false
+        reachedFirstBorder = null,
+        reachedAllBorders = null,
+        reachedIterationCount = null
     } = {}){
         this.Instance.StopCondition = {
             maxCircles: maxCircles,
-            hitOneBorder: hitOneBorder,
-            hitAllBorders: hitAllBorders,
-            includeCoordsOutsideBorders: includeCoordsOutsideBorders
+            reachedFirstBorder: reachedFirstBorder,
+            reachedAllBorders: reachedAllBorders,
+            reachedIterationCount: reachedIterationCount
         };
     }
     
     set StopCondition({
         maxCircles = null,
-        hitOneBorder = false,
-        hitAllBorders = false,
-        includeCoordsOutsideBorders = false
+        reachedFirstBorder = null,
+        reachedAllBorders = null,
+        reachedIterationCount = null
     } = {}){
-        this._maxCircles = maxCircles;
-        this._hitOneBorder = hitOneBorder;
-        this._hitAllBorders = hitAllBorders;
-        this._includeCoordsOutsideBorders = includeCoordsOutsideBorders;
+        if (maxCircles != null) {
+            this._stopCondition.maxCircles = maxCircles;
+        }
+        if (reachedFirstBorder != null) {
+            this._stopCondition.reachedFirstBorder = reachedFirstBorder;
+        }
+        if (reachedAllBorders != null) {
+            this._stopCondition.reachedAllBorders = reachedAllBorders;
+        }
+        if (reachedIterationCount != null) {
+            this._stopCondition.reachedIterationCount = reachedIterationCount;
+        }
+    }
+
+    static set Filter({
+        useCustomFunc = null,
+        customFunc = null
+    } = {}){
+        this.Instance.Filter = {
+            useCustomFunc: useCustomFunc,
+            customFunc: customFunc
+        };
+    }
+
+    set Filter({
+        useCustomFunc = null,
+        customFunc = null
+    } = {}){
+        if (useCustomFunc != null) {
+            this._filter.useCustomFunc = useCustomFunc;
+        }
+        if (customFunc != null) {
+            this._filter.customFunc = customFunc;
+        }
+    }
+
+    static set Border({
+        x = null,
+        y = null,
+        width = null,
+        height = null,
+        includeCoordsOutside = null,
+        leftVerticalX = null,
+        rightVerticalX = null,
+        topHorizontalY = null,
+        bottomHorizontalY = null
+    } = {}){
+        this.Instance.Border = {
+            x: x,
+            y: y,
+            width: width,
+            height: height,
+            includeCoordsOutside: includeCoordsOutside,
+            leftVerticalX: leftVerticalX,
+            rightVerticalX: rightVerticalX,
+            topHorizontalY: topHorizontalY,
+            bottomHorizontalY: bottomHorizontalY
+        };
+    }
+
+    set Border({
+        x = null,
+        y = null,
+        width = null,
+        height = null,
+        includeCoordsOutside = null,
+        leftVerticalX = null,
+        rightVerticalX = null,
+        topHorizontalY = null,
+        bottomHorizontalY = null
+    } = {}){
+        if (x != null) { 
+            this._border.x = x; 
+            this.SetLeftAndRightBorder();
+        }
+        if (y != null) { 
+            this._border.y = y; 
+            this.SetTopAndBottomBorder();
+        }
+        if (width != null) { 
+            this._border.width = width; 
+            this.SetLeftAndRightBorder();
+        }
+        if (height != null) { 
+            this._border.height = height; 
+            this.SetTopAndBottomBorder();
+        }
+        if (includeCoordsOutside != null) {
+            this._border.includeCoordsOutside = includeCoordsOutside;
+        }
+        if (leftVerticalX != null) {
+            this._border.leftVerticalX = leftVerticalX;
+        }
+        if (rightVerticalX != null) {
+            this._border.rightVerticalX = rightVerticalX;
+        }
+        if (topHorizontalY != null) {
+            this._border.topHorizontalY = topHorizontalY;
+        }
+        if (bottomHorizontalY != null) {
+            this._border.bottomHorizontalY = bottomHorizontalY;
+        }
+    }
+
+    static set Walking({
+        direction = null
+    } = {}){
+        this.Instance.Walking = {
+            direction: direction
+        };
+    }
+
+    set Walking({
+        direction = null
+    } = {}){
+        if (direction != null) {
+            this._walking.direction = direction;
+        }
     }
 
         // Line 1 - 12
@@ -85,22 +229,34 @@ export class SpiralWalkCoordGen {
     
     *[Symbol.iterator](){
 
-        this._circle = 0;  
+        this._circleCount = 0;
+        this._coordsCount = 0;
+        const directionX = this._walking.direction == "ccw" ? -1 : 1;
 
-        if (this._isIncludingStartCoor) {
-            yield { x: this._startX, y: this._startY };
+        if (this._startCoord.includeInIteration) {
+            const coord = { x: this._startCoord.x, y: this._startCoord.y };
+            if (this.IncludeCoordinate(coord)) {
+                this._coordsCount++;
+                yield coord;
+            }
         }
+
+        this._spiralBorder.leftX = this._startCoord.x - directionX;
+        this._spiralBorder.rightX = this._spiralBorder.leftX;
+        this._spiralBorder.topY = this._startCoord.y - 1;
+        this._spiralBorder.bottomY = this._spiralBorder.topY;
 
         while (this.CheckStopCondition() == false) {
 
-            this._circle++;
-            const lineLength = this._circle * 2;
-            let linePointX = this._startX - this._circle;
-            let linePointY = this._startY - this._circle;
+            this._circleCount++;
+            const lineLength = this._circleCount * 2;
+            let linePointX = this._startCoord.x - this._circleCount * directionX;
+            let linePointY = this._startCoord.y - this._circleCount;
 
-            for (let hTop = 0; hTop < lineLength; hTop++, linePointX++) {
-                const coord = { x: linePointX + 1, y: linePointY };
+            for (let hTop = 0; hTop < lineLength; hTop++, linePointX += directionX) {
+                const coord = { x: linePointX + directionX, y: linePointY };
                 if (this.IncludeCoordinate(coord)) {
+                    this._coordsCount++;
                     yield coord;
                 }
             }
@@ -108,13 +264,23 @@ export class SpiralWalkCoordGen {
             for (let vRight = 0; vRight < lineLength; vRight++, linePointY++) {
                 const coord = { x: linePointX, y: linePointY + 1 };
                 if (this.IncludeCoordinate(coord)) {
+                    this._coordsCount++;
                     yield coord;
                 }
             }
 
-            for (let hBottom = 0; hBottom < lineLength; hBottom++, linePointX--) {
-                const coord = { x: linePointX - 1, y: linePointY };
+            if (this._walking.direction == "ccw") {
+                this._spiralBorder.leftX = linePointX;
+            }
+            else {
+                this._spiralBorder.rightX = linePointX;
+            }
+            this._spiralBorder.bottomY = linePointY;
+
+            for (let hBottom = 0; hBottom < lineLength; hBottom++, linePointX -= directionX) {
+                const coord = { x: linePointX - directionX, y: linePointY };
                 if (this.IncludeCoordinate(coord)) {
+                    this._coordsCount++;
                     yield coord;
                 }
             }
@@ -122,9 +288,18 @@ export class SpiralWalkCoordGen {
             for (let vLeft = 0; vLeft < lineLength; vLeft++, linePointY--) {
                 const coord = { x: linePointX, y: linePointY - 1 };
                 if (this.IncludeCoordinate(coord)) {
+                    this._coordsCount++;
                     yield coord;
                 }
             }
+
+            if (this._walking.direction == "cw") {
+                this._spiralBorder.leftX = linePointX;
+            }
+            else {
+                this._spiralBorder.rightX = linePointX;
+            }
+            this._spiralBorder.topY = linePointY;
         }      
     }
 
@@ -132,36 +307,53 @@ export class SpiralWalkCoordGen {
         x = null,
         y = null
     } = {}) {
+        if (this._stopCondition.reachedIterationCount !== false && this._coordsCount >= this._stopCondition.reachedIterationCount) {
+            return false;
+        }
+        if (this._filter.useCustomFunc) {
+            return this._filter.customFunc(
+                x, y, this._startCoord.x, this._startCoord.y, this._circleCount,
+                this._border.leftVerticalX, this._border.rightVerticalX,
+                this._border.topHorizontalY, this._border.bottomHorizontalY
+            );
+        }
         return true;
     }
 
     CheckStopCondition() {
-        if (this._maxCircles !== null && this._maxCircles <= this._circle) {
+        if (this._stopCondition.maxCircles !== false && this._circleCount >= this._stopCondition.maxCircles) {
             return true;
         }
-        else {
-            return false;
+        if (this._stopCondition.reachedFirstBorder ) {
+            return this._spiralBorder.leftX <= this._border.leftVerticalX || 
+            this._spiralBorder.rightX >= this._border.rightVerticalX ||
+            this._spiralBorder.topY <= this._border.topHorizontalY ||
+            this._spiralBorder.bottomY >= this._border.bottomHorizontalY;
         }
-
-        if (this._hitOneBorder) {
-            if (x === 0 || x === this._boardWidth - 1 || y === 0 || y === this._boardHeight - 1) {
-                return true;
-            }
+        if (this._stopCondition.reachedAllBorders) {
+            return this._spiralBorder.leftX <= this._border.leftVerticalX &&
+            this._spiralBorder.rightX >= this._border.rightVerticalX &&
+            this._spiralBorder.topY <= this._border.topHorizontalY &&
+            this._spiralBorder.bottomY >= this._border.bottomHorizontalY;
         }
-
-        if (this._hitAllBorders) {
-            if (x < 0 || x >= this._boardWidth || y < 0 || y >= this._boardHeight) {
-                return true;
-            }
+        if (this._stopCondition.reachedIterationCount !== false && this._coordsCount >= this._stopCondition.reachedIterationCount) {
+            return true;
         }
-
-        if (this._includeCoordsOutsideBorders) {
-            if (x < 0 || x >= this._boardWidth || y < 0 || y >= this._boardHeight) {
-                return false;
-            }
-        }
-
         return false;
+    }
+
+    static Reset() {
+        this._instance = null;
+    }
+
+    SetLeftAndRightBorder() {
+        this._border.leftVerticalX = this._border.x;
+        this._border.rightVerticalX = this._border.x + this._border.width;
+    }
+
+    SetTopAndBottomBorder() {
+        this._border.topHorizontalY = this._border.y;
+        this._border.bottomHorizontalY = this._border.y + this._border.height;
     }
 
 }
--- SpiralWalkCoordGen.test.js
+++ SpiralWalkCoordGen.test.js
@@ -1,3 +1,4 @@
+import { beforeEach } from "node:test";
 import { SpiralWalkCoordGen } from "./SpiralWalkCoordGen"
 
 
@@ -5,7 +6,8 @@ import { SpiralWalkCoordGen } from "./SpiralWalkCoordGen"
 describe("Testing Spiral Walk as static", () => {
 
     test("Testing zero circles, start coordinate included", () => {
-        SpiralWalkCoordGen.StopCondition = { maxCircles: 0};
+        SpiralWalkCoordGen.Reset();
+        SpiralWalkCoordGen.StopCondition = { maxCircles: 0 };
         SpiralWalkCoordGen.StartCoord = {x: 5, y: 5};
 
         const result = [];
@@ -19,6 +21,7 @@ describe("Testing Spiral Walk as static", () => {
     })
 
     test("Testing zero circles, start coordinate excluded", () => {
+        SpiralWalkCoordGen.Reset();
         SpiralWalkCoordGen.StopCondition = { maxCircles: 0};
         SpiralWalkCoordGen.StartCoord = {x: 5, y: 5, includeInIteration: false};
 
@@ -32,9 +35,9 @@ describe("Testing Spiral Walk as static", () => {
     })
 
     test("Testing one circle", () => {
-
+        SpiralWalkCoordGen.Reset();
         SpiralWalkCoordGen.StopCondition = { maxCircles: 1};
-        SpiralWalkCoordGen.StartCoord = {x: 5, y: 5, includeInIteration: true};
+        SpiralWalkCoordGen.StartCoord = {x: 5, y: 5};
 
         const result = [];
 
@@ -52,6 +55,89 @@ describe("Testing Spiral Walk as static", () => {
         expect(result[6]).toStrictEqual({x: 4, y: 6});
         expect(result[7]).toStrictEqual({x: 4, y: 5});
         expect(result[8]).toStrictEqual({x: 4, y: 4});
+    })
+
+    test("Iteration stops after 441 coordinates", () => {
+        SpiralWalkCoordGen.Reset();
+
+        const result = [];
+        for (const coord of SpiralWalkCoordGen) {
+            result.push(coord);
+        }
+
+        expect(result).toHaveLength(441);
+        expect(result[0]).toStrictEqual({x: 0, y: 0});
+        expect(result[440]).toStrictEqual({x: -10, y: -10});
+    })
+
+    test("Iteration stops after 10000 coordinates when turning off border check", () => {
+        SpiralWalkCoordGen.Reset();
+        SpiralWalkCoordGen.StopCondition = { reachedAllBorders: false };
+
+        const result = [];
+        for (const coord of SpiralWalkCoordGen) {
+            result.push(coord);
+        }
+
+        expect(result).toHaveLength(10000);
+        expect(result[0]).toStrictEqual({x: 0, y: 0});
+        expect(result[9999]).toStrictEqual({x: 50, y: 49});
+    })
+
+    test("Custom filter that always return false gives 0 coordinates", () => {
+        SpiralWalkCoordGen.Reset();
+        SpiralWalkCoordGen.Filter = {
+            useCustomFunc: true,
+            customFunc: () => false
+        }
+
+        const result = [];
+
+        for (const coord of SpiralWalkCoordGen) {
+            result.push(coord);
+        }
+
+        expect(result).toHaveLength(0);
+    })
+
+    test("Spiral goes ccw on circle when changing direction", () => {
+        SpiralWalkCoordGen.Reset();
+        SpiralWalkCoordGen.StopCondition = { maxCircles: 1};
+        SpiralWalkCoordGen.StartCoord = {x: 5, y: 5};
+        SpiralWalkCoordGen.Walking = {direction: "ccw"};
+
+        const result = [];
+
+        for (const coord of SpiralWalkCoordGen) {
+            result.push(coord);
+        }
+
+        console.log(result);
+
+        expect(result).toHaveLength(9);
+        expect(result[0]).toStrictEqual({x: 5, y: 5});
+        expect(result[1]).toStrictEqual({x: 5, y: 4});
+        expect(result[2]).toStrictEqual({x: 4, y: 4});
+        expect(result[3]).toStrictEqual({x: 4, y: 5});
+        expect(result[4]).toStrictEqual({x: 4, y: 6});
+        expect(result[5]).toStrictEqual({x: 5, y: 6});
+        expect(result[6]).toStrictEqual({x: 6, y: 6});
+        expect(result[7]).toStrictEqual({x: 6, y: 5});
+        expect(result[8]).toStrictEqual({x: 6, y: 4});
+    })
+
+    test("Iteration stops after 441 coordinates in ccw direction", () => {
+        SpiralWalkCoordGen.Reset();
+        SpiralWalkCoordGen.Walking = { direction: "ccw" };
+
+        const result = [];
+        for (const coord of SpiralWalkCoordGen) {
+            result.push(coord);
+        }
+
+        expect(result).toHaveLength(441);
+        expect(result[0]).toStrictEqual({x: 0, y: 0});
+        expect(result[440]).toStrictEqual({x: 10, y: -10});
     })
 
 })

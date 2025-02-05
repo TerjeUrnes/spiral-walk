c7ffd00822827984b02e6457cd29bd12cf315aed
Terje Urnes <terje@urnes.net>
2025-02-05 01:14:08 +0100

    Extra test

--- SpiralWalkCoordGen.test.js
+++ SpiralWalkCoordGen.test.js
@@ -140,4 +140,39 @@ describe("Testing Spiral Walk as static", () => {
         expect(result[440]).toStrictEqual({x: 10, y: -10});
     })
 
+    test("Changing only x in StartCoord will shift result to right", () => {
+        SpiralWalkCoordGen.Reset();
+        SpiralWalkCoordGen.Border = {
+            x: 0,
+            y: 0,
+            width: 100,
+            height: 100
+        }
+        SpiralWalkCoordGen.StopCondition = { reachedIterationCount: 5};
+        SpiralWalkCoordGen.StartCoord = {x: 60, y: 39};
+
+        const resultA = [];
+        for (const coord of SpiralWalkCoordGen) {
+            resultA.push(coord);
+        }
+
+        SpiralWalkCoordGen.StartCoord = {x: 75 };
+
+        const resultB = [];
+        for (const coord of SpiralWalkCoordGen) {
+            resultB.push(coord);
+        }
+
+        expect(resultA[0].x + 15).toEqual(resultB[0].x);
+        expect(resultA[0].y).toEqual(resultB[0].y);
+        expect(resultA[1].x + 15).toEqual(resultB[1].x);
+        expect(resultA[1].y).toEqual(resultB[1].y);
+        expect(resultA[2].x + 15).toEqual(resultB[2].x);
+        expect(resultA[2].y).toEqual(resultB[2].y);
+        expect(resultA[3].x + 15).toEqual(resultB[3].x);
+        expect(resultA[3].y).toEqual(resultB[3].y);
+        expect(resultA[4].x + 15).toEqual(resultB[4].x);
+        expect(resultA[4].y).toEqual(resultB[4].y);
+    })
+
 })
